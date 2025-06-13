52e9ebf0a1f874fb3fbe08eb4d12bda5d9471f63
strhzy <alexeydm2@gmail.com>
2025-06-13 16:48:15 +0300

    добавлены формы для круд

--- django_test/firstapp/models.py
+++ django_test/firstapp/models.py
@@ -104,3 +104,15 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.product.name} (x{self.quantity})"
 
+class CartItem(models.Model):
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name="Покупатель")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за единицу")
+
+    class Meta:
+        verbose_name = "Элемент корзины"
+        verbose_name_plural = "Элементы корзины"
+
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity})"
