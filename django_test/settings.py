892c7e4a93bd3da447f04d72d99be0fc86ea2b40
strhzy <alexeydm2@gmail.com>
2025-06-19 09:10:49 +0300

    итог + gunicorn

--- django_test/settings.py
+++ django_test/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-693oi3i&%n8tc8um2k1*xqukl3+bh!wd-fioa1d@#9mvado%c%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGIN_URL = '/login'
 
